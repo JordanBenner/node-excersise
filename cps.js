e2dbbdcacc5465fbc71f1aa9625119a411e99fd0
Jordan Benner <jordanbenner@outlook.com>
2017-05-30 13:33:24 -0500

    Problem 3

--- cps.js
+++ cps.js
@@ -7,3 +7,12 @@ f(x, y function(z)){
 g(a, b, c function(y)){
   console.log(y);
 });
+
+//problem 3
+
+function z(numbers, callback){
+  var result = numbers.convert(function(a, b, c, d, e){
+    return a + b + c + d + e;
+  }, 0);
+  callback(result);
+}
