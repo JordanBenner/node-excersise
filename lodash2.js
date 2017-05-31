17b7843c4ac8d3c0536d2afedb0354bf038137a5
Jordan Benner <jordanbenner@outlook.com>
2017-05-31 15:41:31 -0500

    For each key

--- lodash2.js
+++ lodash2.js
@@ -1,16 +1,19 @@
-//sum function
-var sum = require('lodash/sum');
-console.log(sum([1, 3, 9]));
-
-//max function
-var max = require('lodash/max');
-console.log(max([4, 2, 5, 8]));
-//mean function
-var mean = require('lodash/mean');
-console.log(mean([10, 2, 5, 8, 6, 4, 3]));
+// //sum function
+// var sum = require('lodash/sum');
+// console.log(sum([1, 3, 9]));
+//
+// //max function
+// var max = require('lodash/max');
+// console.log(max([4, 2, 5, 8]));
+// //mean function
+// var mean = require('lodash/mean');
+// console.log(mean([10, 2, 5, 8, 6, 4, 3]));
 
 //forEach function
 var forEach = require('lodash/foreach');
-.forEach([1, 2], function(value) {
+forEach([1, 2], function(value) {
   console.log(value);
+});
+forEach({ 'a': 1, 'b': 2 }, function(value, key) {
+  console.log(key);
 });
