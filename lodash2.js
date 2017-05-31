ded572d0c288069d5eb2b887d51b3b6ade73505a
Jordan Benner <jordanbenner@outlook.com>
2017-05-31 15:39:29 -0500

    For each

--- lodash2.js
+++ lodash2.js
@@ -8,3 +8,9 @@ console.log(max([4, 2, 5, 8]));
 //mean function
 var mean = require('lodash/mean');
 console.log(mean([10, 2, 5, 8, 6, 4, 3]));
+
+//forEach function
+var forEach = require('lodash/foreach');
+.forEach([1, 2], function(value) {
+  console.log(value);
+});
