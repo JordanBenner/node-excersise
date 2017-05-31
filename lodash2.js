53ebd5357c8082a1d9809d61410b843871b3437e
Jordan Benner <jordanbenner@outlook.com>
2017-05-31 15:36:02 -0500

    Function notes

--- lodash2.js
+++ lodash2.js
@@ -1,10 +1,10 @@
-// var sum = require('lodash/sum');
-//
-// console.log(sum([1, 3, 9]));
+//sum function
+var sum = require('lodash/sum');
+console.log(sum([1, 3, 9]));
 
-
-// var max = require('lodash/max');
-// console.log(max([4, 2, 5, 8]));
-
+//max function
+var max = require('lodash/max');
+console.log(max([4, 2, 5, 8]));
+//mean function
 var mean = require('lodash/mean');
 console.log(mean([10, 2, 5, 8, 6, 4, 3]));
