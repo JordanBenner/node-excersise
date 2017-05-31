788d3aabc97017718fe27d6749016bc9461fe79f
Jordan Benner <jordanbenner@outlook.com>
2017-05-31 14:24:27 -0500

    Added express

--- npm.js
+++ npm.js
@@ -69,3 +69,39 @@ do_stuff(function (error, result) {
 });
 
 console.log('END OF FILE');
+
+
+//module
+
+
+var readline = require('./fileread');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function do_stuff (callback) {
+  rl.question("Reverse what file?", function (filename) {
+    fs.readFile(filename, function (error, buffer) {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      var content = buffer.toString();
+      content = content.split('');
+      content = content.reverse();
+      content = content.join('');
+      fs.writeFile('reverse.txt', content, function (error) {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+
+        callback(null, 'File Saved!');
+      });
+    });
+  });
+}
+exports.do_stuff = do_stuff;
