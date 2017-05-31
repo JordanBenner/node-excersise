e27c24ad7f604e9620d8a0c3e1639aa781553961
Jordan Benner <jordanbenner@outlook.com>
2017-05-31 15:34:38 -0500

    Mean module

--- lodash2.js
+++ lodash2.js
@@ -3,5 +3,8 @@
 // console.log(sum([1, 3, 9]));
 
 
-var max = require('lodash/max');
-console.log(max([4, 2, 5, 8]))
+// var max = require('lodash/max');
+// console.log(max([4, 2, 5, 8]));
+
+var mean = require('lodash/mean');
+console.log(mean([10, 2, 5, 8, 6, 4, 3]));
